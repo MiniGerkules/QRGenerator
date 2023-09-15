23c955bdf605050b1330356fd31192802e201cf4
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-09-15 22:24:14 +0300

    Change extension type and add 0-s to the beginning of the binary string

--- QRCodeGenerator/Extensions/IntExtensions.swift
+++ QRCodeGenerator/Extensions/IntExtensions.swift
@@ -5,12 +5,15 @@
 //  Created by Евгений Кацер on 11.09.2023.
 //
 
-extension Int {
-    func toBinString(numOfBinDigits: Int) -> String.SubSequence? {
-        let numBitsInInt = MemoryLayout<Int>.size * 8
-        guard numOfBinDigits <= numBitsInInt else { return nil }
+extension BinaryInteger {
+    func toBinString() -> String {
+        let numBits = MemoryLayout.size(ofValue: self) * 8
+        var binStr = String(self, radix: 2)
 
-        let drop = numBitsInInt - numOfBinDigits
-        return String(self, radix: 2).dropFirst(drop)
+        let diff = numBits - binStr.count
+        binStr.insert(contentsOf: String(repeating: "0", count: diff),
+                      at: binStr.startIndex)
+
+        return binStr
     }
 }
