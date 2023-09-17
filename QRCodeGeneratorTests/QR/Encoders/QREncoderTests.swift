5cd6054f071fe718c29574124bff9e8dd236b04c
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-09-17 20:31:13 +0300

    Fix test for QREncoder

--- QRCodeGeneratorTests/QR/Encoders/QREncoderTests.swift
+++ QRCodeGeneratorTests/QR/Encoders/QREncoderTests.swift
@@ -12,10 +12,9 @@ final class QREncoderTests: XCTestCase {
     private let encoder_ = NumsQREncoder()
 
     func testGenerateQRData() {
-        let arrMustBe: QRData = [16, 36, 123, 114, 49, 80, 236, 17, 236, 17, 236, // Data
-                                 17, 236, 17, 236, 17, 236, 17, 236,
-                                 124, 242, 180, 34, 68, 5, 233, 0, 0, 0, 0, 0, 0, // Correction
-                                 0, 0, 0, 0, 0, 0]
+        let arrMustBe: QRData = [16, 36, 123, 114, 49, 80, 236, 17, 236, 17, 236,   // Data
+                                 17, 236, 17, 236, 17, 236, 17, 236,                // Data
+                                 124, 242, 180, 34, 68, 5, 233]                     // Correction
 
         guard let result = try? encoder_.generateQRData("123456789", with: .L) else {
             XCTFail("Must be not optional!")
