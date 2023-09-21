2943b8930066a074d398beb7097c34d80b294de5
Eugene Katser <62696210+MiniGerkules@users.noreply.github.com>
2023-09-22 00:47:42 +0300

    Fix one test for QRCodeGenerator

--- QRCodeGeneratorTests/QR/QRCodeGeneratorTests.swift
+++ QRCodeGeneratorTests/QR/QRCodeGeneratorTests.swift
@@ -36,6 +36,9 @@ final class QRCodeGeneratorTests: XCTestCase {
             [w, w, w, w,    b, w, w, w, w, w, b, w, b, w, b, w, w, b, w, w, b, w, b, b, b,    w, w, w, w],
             [w, w, w, w,    b, w, b, b, b, w, b, w, b, b, w, b, w, w, b, w, w, w, b, b, b,    w, w, w, w],
             [w, w, w, w,    b, w, b, b, b, w, b, w, b, w, b, b, b, w, w, w, b, w, b, w, w,    w, w, w, w],
+            [w, w, w, w,    b, w, b, b, b, w, b, w, w, b, w, w, w, w, b, w, w, w, w, b, b,    w, w, w, w],
+            [w, w, w, w,    b, w, w, w, w, w, b, w, b, b, b, w, w, b, b, b, w, w, b, b, w,    w, w, w, w],
+            [w, w, w, w,    b, b, b, b, b, b, b, w, w, b, w, b, w, w, w, w, w, w, w, b, w,    w, w, w, w],
 
             [w, w, w, w,    w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w,    w, w, w, w],
             [w, w, w, w,    w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w,    w, w, w, w],
